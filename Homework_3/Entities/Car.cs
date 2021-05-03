c6c59d3d806b8d96745b193b611a73b377020f38
Elena-r-code <75211054+Elena-r-code@users.noreply.github.com>
2021-05-03 15:45:24 +0200

    Add files via upload

---
+++ Homework_3/Entities/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class Car :Vehicle
+    {
+        public int FuelTank { get; set; }
+        public string Countries { get; set; }
+
+        public Car(int id, string type, int year, int batch, int fuel, string countries) : base(id, type, year, batch)
+        {
+            FuelTank = fuel;
+            Countries = countries;
+        }
+
+        public void PrintVehicle()
+        {
+            Console.WriteLine($"The vehicle of type {Type} is produced in {Countries}");
+        }
+    }
+}
