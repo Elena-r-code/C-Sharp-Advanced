694d5ecd0b572ec6b966e210fb6a481f170a9f66
Elena-r-code <75211054+Elena-r-code@users.noreply.github.com>
2021-06-01 23:21:10 +0200

    Add files via upload

---
+++ Homework_8/SerializingAndDeserializing/Task1/DataBase/DataBase.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using Task1.Domain;
+
+
+namespace Task1
+{
+    public class DataBase
+    {
+        private string _folderPath;
+        private string _filePath;
+        public DataBase()
+        {
+            _folderPath = @"..\..\..\Data";
+            _filePath = _folderPath + $"\\Dogs.json";
+
+            if (!Directory.Exists(_folderPath))
+            {
+                Directory.CreateDirectory(_folderPath);
+            }
+
+            if (!File.Exists(_filePath))
+            {
+                File.Create(_filePath).Close();
+            }
+            Dog[] data = ReadFileContent();
+            if(data == null)
+            {
+                Dog[] dogs = new Dog[0] { };
+                WriteInFile(dogs);
+            }
+        }
+
+        private Dog[] ReadFileContent()
+        {           
+
+            using (StreamReader streamReader = new StreamReader(_filePath))
+            {
+                string result = streamReader.ReadToEnd();
+                return JsonConvert.DeserializeObject<Dog[]>(result);
+            }
+            
+        }
+
+        private void WriteInFile(Dog[] dogs)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(_filePath))
+            {
+
+                string jsonString = JsonConvert.SerializeObject(dogs);
+                streamWriter.WriteLine(jsonString);
+            }
+        }
+
+        public void Insert(Dog dog)
+        {
+            Dog[] data = ReadFileContent();
+            Array.Resize(ref data, data.Length + 1);
+            data[data.Length - 1] = dog;
+            WriteInFile(data);
+        }
+
+        public void GetAll()
+        {
+            foreach (Dog dog in ReadFileContent())
+            {
+                Console.WriteLine(dog.PrintInfo());
+            }
+        }
+    }
+}
---
+++ Homework_8/SerializingAndDeserializing/Task1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Task1.Domain;
+
+namespace Task1
+{
+    class Program
+    {
+        public static DataBase _database = new DataBase();
+
+        static void Menu()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Please enter the following information for the dog :\n");
+            Console.WriteLine("Name : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("\nAge : ");
+            bool success = int.TryParse(Console.ReadLine(), out int age);
+            if (!success)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n\n\n     Please enter a number!!!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("\nColor : ");
+                string color = Console.ReadLine();
+
+                Dog dog = new Dog(name, age, color);
+                _database.Insert(dog);
+                Console.WriteLine("This dog is now added in the list! Check the file! :)");
+            }
+           
+        }
+
+        static void Repeat()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(" \n\n\n\n\n\n                   HELLO ! CHOOSE OPTION : \n\n\n\n\n\n            1. Add dog to the list \n\n            2. See the whole list");
+                bool success1 = int.TryParse(Console.ReadLine(), out int answer);
+                if (!success1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error!  Plese choose one of the available options!");
+                }
+                else if(answer == 1)
+                {
+                    Console.Clear();
+                    Menu();
+                    Console.WriteLine("Press anything to continue...");
+                    Console.ReadKey();
+                    break;
+                }
+                else if(answer == 2)
+                {
+                    Console.Clear();
+                    _database.GetAll();
+                    Console.WriteLine("Press anything to continue...");
+                    Console.ReadKey();
+                    break;
+                }
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Repeat();
+                Console.ReadKey();
+            }
+            Console.ReadLine();
+        }
+    }
+}
